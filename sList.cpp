12d1e09bf112b8ce6ef42d0aaf50da6f3b8e6034
abady944 <44455438+abady944@users.noreply.github.com>
2019-10-15 21:40:18 -0700

    Update sList.cpp

--- sList.cpp
+++ sList.cpp
@@ -1,3 +1,10 @@
+/*
+- Name: Abdullah Banawas
+- Class: CS441 Software Engineer
+- Group Project ( Cocktails Recipes )
+	- Shopping List Class implementaion 
+- Date: October 15, 2019
+*/
 #include <iostream>
 #include <vector>
 #include "sList.h"
