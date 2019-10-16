4664164635c80938eee4a7e3a0b7050842334f6a
abady944 <44455438+abady944@users.noreply.github.com>
2019-10-16 16:56:40 -0700

    Update sList.cpp

--- sList.cpp
+++ sList.cpp
@@ -2,7 +2,7 @@
 - Name: Abdullah Banawas
 - Class: CS441 Software Engineer
 - Group Project ( Cocktails Recipes )
-	- Shopping List Class implementaion 
+    - Shopping List Class implementaion
 - Date: October 15, 2019
 */
 #include <iostream>
@@ -17,27 +17,26 @@ sList::~sList() {}
 
 // add ingredient to shopping list
 void sList::addIngredient(Ingredient name) {
-	list.push_back(name);  // add ingredient into shopping list vector
+    list.push_back(name);  // add ingredient into shopping list vector
 }
 
 // remove an item from the list
 void sList::removeIngredient(Ingredient ing) {
-	for (int i = 0; i < list.size(); i++)
-	{
-		// compare specfied ingredient with list ingredient
-		// if a match was found then erase it
-		if( ing.name == list[i].name) // *this may not work*		
-			list.erase(list.begin() + i); // earise i+1 element
-	}
+    for (int i = 0; i < list.size(); i++)
+    {
+        // compare specfied ingredient with list ingredient
+        // if a match was found then erase it
+        if( ing.getName() == list[i].getName()) // *this may not work*
+            list.erase(list.begin() + i); // earise i+1 element
+    }
 }
-
 // display all items in the list with the total cost
 void sList::display() {
-	double totalCost = 0; // add up the costs of all ingredients
-	for (int i = 0; i < list.size(); i++) // for loop goes through vectore and print the info
-	{
-		cout << list[i].name << "  " << list[i].cost << endl; // display ingredient name + cost
-		totalCost += list[i].cost; // add cost of each ingredient to totalCost
-	}
-	cout << "\nThe total cost of the igredients: $" << totalCost;
+    double totalCost = 0; // add up the costs of all ingredients
+    for (int i = 0; i < list.size(); i++) // for loop goes through vectore and print the info
+    {
+        cout << list[i].getName() << "  " << list[i].getPrice() << endl; // display ingredient name + cost
+        totalCost += list[i].getPrice(); // add cost of each ingredient to totalCost
+    }
+    cout << "\nThe total cost of the igredients: $" << totalCost;
 }
